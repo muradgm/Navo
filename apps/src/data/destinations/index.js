bac58c860b10dbc2c75a68be33f9feb4d3d31311
Murad Muqbel <59296531+muradgm@users.noreply.github.com>
2026-07-06 10:31:43 +0200

    feat: support persisted destination selection

--- apps/src/data/destinations/index.js
+++ apps/src/data/destinations/index.js
@@ -1,6 +1,9 @@
 import { baselDestinationPack } from "./basel/index.js";
 import { barcelonaDestinationPack } from "./barcelona/index.js";
 
+const FALLBACK_DESTINATION_ID = "basel";
+const DESTINATION_STORAGE_KEY = "navo-active-destination-id";
+
 export const destinationPacks = {
   basel: baselDestinationPack,
   barcelona: barcelonaDestinationPack,
@@ -8,9 +11,24 @@ export const destinationPacks = {
 
 export const destinationIds = Object.keys(destinationPacks);
 export const destinationRegistry = destinationPacks;
-export const defaultDestinationId = "basel";
-export const defaultDestinationPack = baselDestinationPack;
+
+function getInitialDestinationId() {
+  if (typeof window === "undefined") return FALLBACK_DESTINATION_ID;
+
+  const params = new URLSearchParams(window.location.search);
+  const requestedDestination =
+    params.get("destination") ||
+    window.localStorage.getItem(DESTINATION_STORAGE_KEY) ||
+    FALLBACK_DESTINATION_ID;
+
+  return destinationRegistry[requestedDestination]
+    ? requestedDestination
+    : FALLBACK_DESTINATION_ID;
+}
+
+export const defaultDestinationId = getInitialDestinationId();
+export const defaultDestinationPack = destinationRegistry[defaultDestinationId];
 
 export function getDestinationPack(id) {
-  return destinationRegistry[id] || defaultDestinationPack;
+  return destinationRegistry[id] || destinationRegistry[FALLBACK_DESTINATION_ID];
 }
